6b14999fdb4f2597a62c8365e59c4528fd19d987
itpark-team <46758840+itpark-team@users.noreply.github.com>
2023-07-08 12:43:21 +0300

    Add delete

--- WpfAppCookBookMongoDb/Repositories/HomefoodRepository.cs
+++ WpfAppCookBookMongoDb/Repositories/HomefoodRepository.cs
@@ -19,37 +19,9 @@ namespace WpfAppCookBookMongoDb.Repositories
         }
 
 
-        public void AddNew()
+        public void AddNew(FoodRecipe foodRecipe)
         {
-            FoodRecipe foodRecipe = new FoodRecipe()
-            {
-                FoodCost = (decimal)23.1,
-                Rating = (decimal)4.5,
-                Name = "Апельсиновый тарт",
-                PicturePath = "apelsinovyj-desert.jpg",
-                Ingredients = new List<Ingredient>()
-                {
-                    new Ingredient()
-                    {
-                        Id = new ObjectId(),
-                        Name = "Мука",
-                        Amount = "2 с.л."
-                    },
-                    new Ingredient()
-                    {
-                        Id = new ObjectId(),
-                        Name = "Яйца",
-                        Amount = "3 шт."
-                    },
-                    new Ingredient()
-                    {
-                        Id = new ObjectId(),
-                        Name = "Сахар",
-                        Amount = "4 ч.л."
-                    },
-                }
-            };
-
+           
             _foodRecipes.InsertOne(foodRecipe);
         }
 
@@ -58,5 +30,9 @@ namespace WpfAppCookBookMongoDb.Repositories
            return _foodRecipes.Find(new BsonDocument()).ToList();
         }
 
+        public void DeleteOne(FoodRecipe foodRecipe)
+        {
+            _foodRecipes.DeleteOne(item => item.Id == foodRecipe.Id);
+        }
     }
 }
--- WpfAppCookBookMongoDb/ViewModels/MainWindowViewModel.cs
+++ WpfAppCookBookMongoDb/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfAppCookBookMongoDb.Db;
 using WpfAppCookBookMongoDb.Models;
 using WpfAppCookBookMongoDb.Repositories;
@@ -19,10 +22,78 @@ namespace WpfAppCookBookMongoDb.ViewModels
         {
             _homefoodRepository = new HomefoodRepository(ConnetionManager.Instance.GetHomefoodCollection());
 
-            FoodRecipes = _homefoodRepository.GetAll();
+            FoodRecipes = new ObservableCollection<FoodRecipe>(_homefoodRepository.GetAll());
+
+            FoodRecipeForAdd = new FoodRecipe()
+            {
+                FoodCost = (decimal)23.1,
+                Rating = (decimal)4.5,
+                Name = "Заполни меня",
+                PicturePath = "http://www.topglobus.ru/skin/recepty/h/kulinarnyj-recept-apelsinovyj-desert.jpg",
+                Ingredients = new List<Ingredient>()
+                {
+                    new Ingredient()
+                    {
+                        Id = new ObjectId(),
+                        Name = "Мука",
+                        Amount = "2 с.л."
+                    },
+                    new Ingredient()
+                    {
+                        Id = new ObjectId(),
+                        Name = "Яйца",
+                        Amount = "3 шт."
+                    },
+                    new Ingredient()
+                    {
+                        Id = new ObjectId(),
+                        Name = "Сахар",
+                        Amount = "4 ч.л."
+                    },
+                }
+            };
         }
 
         [ObservableProperty]
-        public List<FoodRecipe> _foodRecipes;
+        public ObservableCollection<FoodRecipe> _foodRecipes;
+
+        [ObservableProperty]
+        public FoodRecipe _selectedFoodRecipe;
+
+        [ObservableProperty]
+        public FoodRecipe _foodRecipeForAdd;
+
+        [RelayCommand]
+        public void TestMethod()
+        {
+            MessageBox.Show(FoodRecipeForAdd.Name);
+        }
+
+        [RelayCommand]
+        public void AddNewRecipe()
+        {
+
+            FoodRecipe insertRecipe = new FoodRecipe()
+            {
+                FoodCost = FoodRecipeForAdd.FoodCost,
+                Name = FoodRecipeForAdd.Name,
+                PicturePath = FoodRecipeForAdd.PicturePath,
+                Rating = FoodRecipeForAdd.Rating,
+                Ingredients = new List<Ingredient>(FoodRecipeForAdd.Ingredients)
+            };
+
+            _homefoodRepository.AddNew(insertRecipe);
+
+            FoodRecipes.Add(insertRecipe);
+        }
+
+        [RelayCommand]
+        public void DeleteCurrentRecipe()
+        {
+            if (SelectedFoodRecipe == null) { return; }
+            _homefoodRepository.DeleteOne(SelectedFoodRecipe);
+
+            FoodRecipes.Remove(SelectedFoodRecipe);
+        }
     }
 }
